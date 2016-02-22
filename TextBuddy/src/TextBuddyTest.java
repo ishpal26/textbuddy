ed1cb894b90fec049f551899d0fd77454df98024
ishpal26 <ishpalsinghgill@live.co.uk>
2016-02-22 13:26:18 +0800

    modified testAdd to cover all cases

--- TextBuddy/src/TextBuddyTest.java
+++ TextBuddy/src/TextBuddyTest.java
@@ -15,7 +15,7 @@ public class TextBuddyTest {
 	public static final String TEXT5 = "Cookies n cream";
 	public static final String TEXT6 = "Elephant";
 	public static final String TEXT7 = "Night hawk";
-	public static final String TEXT8 = "Arsenal";
+	public static final String TEXT8 = "Apple";
 	public static final String TESTFILENAME = "file.txt";
 	
 	@BeforeClass
@@ -23,18 +23,6 @@ public class TextBuddyTest {
 		String[] argArray = new String[1];
 		argArray[0] = TESTFILENAME;
 		TextBuddy.checkFile(argArray);
-	}
-	
-	@Test
-	public void invalidCommand() {
-		assertEquals(false, TextBuddy.checkValid("hello"));
-		assertEquals(true, TextBuddy.checkValid("add"));
-		assertEquals(true, TextBuddy.checkValid("display"));
-		assertEquals(false, TextBuddy.checkValid("world"));
-		assertEquals(true, TextBuddy.checkValid("exit"));
-		assertEquals(true, TextBuddy.checkValid("delete"));
-		assertEquals(false, TextBuddy.checkValid("negative"));
-		assertEquals(false, TextBuddy.checkValid(""));
 	}
 	
 	@Test
@@ -48,11 +36,12 @@ public class TextBuddyTest {
 	@Test
 	public void testAdd() throws Exception{
 		TextBuddy.executeCommand("clear");
-		assertEquals("added to file.txt: \""+TEXT1+"\"", TextBuddy.executeCommand("add "+ TEXT1));
-		assertEquals("added to file.txt: \""+TEXT2+"\"", TextBuddy.executeCommand("add "+ TEXT2));
-		assertEquals("added to file.txt: \""+TEXT3+"\"", TextBuddy.executeCommand("add "+ TEXT3));
+		assertEquals("Error: Unrecognized command \"add  \"", TextBuddy.addEntry("add  "));
+		assertEquals("added to file.txt: \""+TEXT1+"\"", TextBuddy.addEntry("add " + TEXT1));
+		assertEquals("added to file.txt: \""+TEXT2+"\"", TextBuddy.addEntry("add " + TEXT2));
+		assertEquals("added to file.txt: \""+TEXT3+"\"", TextBuddy.addEntry("add " + TEXT3));
 		assertEquals(3, TextBuddy.getLineCount());
-		assertEquals("Error: Duplicate content \""+TEXT3+"\" detected", TextBuddy.executeCommand("add " + TEXT3));
+		assertEquals("Error: Duplicate content \""+TEXT3+"\" detected", TextBuddy.addEntry("add " +TEXT3));
 		assertEquals(3, TextBuddy.getLineCount());
 	}	
 	
@@ -82,16 +71,17 @@ public class TextBuddyTest {
 	
 	@Test
 	public void invalidDelete(){
-		assertFalse(TextBuddy.canDelete(-10, 3));
-		assertFalse(TextBuddy.canDelete(-1, 3));
-		assertTrue(TextBuddy.canDelete(1, 3));
-		assertFalse(TextBuddy.canDelete(1000, 3));
-		assertTrue(TextBuddy.canDelete(2, 3));
-		assertFalse(TextBuddy.canDelete(0, 3));
+		assertFalse(TextBuddy.validDeleteIndex(-10, 3));
+		assertFalse(TextBuddy.validDeleteIndex(-1, 3));
+		assertTrue(TextBuddy.validDeleteIndex(1, 3));
+		assertFalse(TextBuddy.validDeleteIndex(1000, 3));
+		assertTrue(TextBuddy.validDeleteIndex(2, 3));
+		assertFalse(TextBuddy.validDeleteIndex(0, 3));
 	}
 	
 	@Test
 	public void testSort() throws Exception{
+		assertEquals(0, TextBuddy.getLineCount());
 		addLines();
 		TextBuddy.sortList();
 		assertEquals(TEXT8, TextBuddy.getListContent(0));
@@ -102,9 +92,14 @@ public class TextBuddyTest {
 		assertEquals(TEXT7, TextBuddy.getListContent(5));
 		assertEquals(TEXT4, TextBuddy.getListContent(6));
 		assertEquals(TEXT2, TextBuddy.getListContent(7));
+		assertEquals(8, TextBuddy.getLineCount());
 	}
 	
-	
+	@Test
+	public void testSearch(){
+		
+	}
+
 	public void addLines() throws Exception{
 		TextBuddy.executeCommand("add " + TEXT1 );
 		TextBuddy.executeCommand("add " + TEXT2 );
